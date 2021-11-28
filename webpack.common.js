953f0bf8bf85bc33bd43e21b2b462feda62e52ea
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-11-28 17:53:39 +0300

    4

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,15 @@
 /* eslint-disable linebreak-style */
 const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const webpack = require('webpack');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   target: 'web',
-  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, '/dist'),
-    open: true,
-    compress: true,
+    publicPath: '/',
   },
   module: {
     rules: [
@@ -42,6 +34,10 @@ module.exports = {
           MiniCssExtractPlugin.loader, 'css-loader',
         ],
       },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
@@ -53,6 +49,5 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
-    new webpack.HotModuleReplacementPlugin(),
   ],
 };

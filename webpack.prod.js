9ad4f3f8f508612677924bfee7c26afa1d461b78
Voinov Nikita <nikita-nikita2012@mail.ru>
2021-12-05 15:36:48 +0300

    fix 2

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,19 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
 const { merge } = require('webpack-merge');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common');
 // const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
+      // eslint-disable-next-line no-undef
       new TerserPlugin({}),
       new OptimizeCSSAssetsPlugin({}),
     ],
